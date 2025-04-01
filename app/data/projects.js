005a911df0229eab0cbcbc784184acd044451c22
Mikka Ella <ellajugz20@gmail.com>
2025-04-01 10:04:41 +0800

    update project

--- app/data/projects.js
+++ app/data/projects.js
@@ -5,7 +5,7 @@ export const projectList = [
     des: 'A simple web app that helps farmers track crops, get real-time prices, and connect directly with buyers, with offline features for remote areas.',
     tech: ['MongDb', 'Express.js', 'React', 'Node.js'],
     url: '',
-    image: '/images/projects/taskeasy.png'
+    image: '/images/projects/anisave.png'
   },
   {
     id: 2,
